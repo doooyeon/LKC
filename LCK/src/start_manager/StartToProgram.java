53119408df4eb47e742222f23ff14b2cf08f192a
Administrator <Administrator@LG-48>
2016-09-28 05:49:03 +0900

    야매

--- LCK/src/start_manager/StartToProgram.java
+++ LCK/src/start_manager/StartToProgram.java
@@ -4,11 +4,15 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.concurrent.TimeoutException;
 
 import client_manager.LinKlipboardClient;
 import server_manager.LinKlipboard;
@@ -40,7 +44,7 @@ public class StartToProgram {
 
 		startProgram();
 	}
-	
+
 	public StartToProgram(LinKlipboardClient client) {
 		this.client = client;
 		this.groupName = LinKlipboardClient.getGroupName();
@@ -67,6 +71,7 @@ public class StartToProgram {
 			// 호출할 서블릿의 URL
 			URL url = new URL(LinKlipboard.URL_To_CALL + servletName);
 			URLConnection conn = url.openConnection();
+			System.out.println(conn);
 
 			// servlet의 doPost호출
 			conn.setDoOutput(true);
@@ -81,39 +86,40 @@ public class StartToProgram {
 			bout.close();
 
 			// 서버로부터 받을 데이터(응답정보)
-			BufferedReader bin = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			InputStream is = conn.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader bin = new BufferedReader(isr);
 
 			if ((response = bin.readLine()) != null) {
 				// 서버에서 확인 후 클라이언트가 받은 결과 메세지
 				this.response = response;
 			}
-			
-//			// 상훈 TEST 시작
-//			// 서버에 보낼 데이터(그룹정보)
-//			String message = "doy";
-//
-//			// server에 그룹이름과 패스워드 전송(servlet이 받는 구분자: &)
-//			bout.write(message);
-//			bout.flush();
-//			bout.close();
+
+			// // 상훈 TEST 시작
+			// // 서버에 보낼 데이터(그룹정보)
+			// String message = "doy";
+			//
+			// // server에 그룹이름과 패스워드 전송(servlet이 받는 구분자: &)
+			// bout.write(message);
+			// bout.flush();
+			// bout.close();
 			// 상훈 TEST 끝
-			
+
 			bin.close();
-					
 
 			exceptionHandling(this.response);
 			System.out.println("[sendGroupInfoToServer] " + ResponseHandler.getErrorCodeNum());
 
 			if (ResponseHandler.getErrorCodeNum() == LinKlipboard.ACCESS_PERMIT) {
-//				if (orderMsg.equals("create")) {
-//					LinKlipboardClient.setHistory();
-//					
-//				} else if (orderMsg.equals("join")) {
-//					// 서버에 있는 Vector<Contents>를 받는다.
-//					LinKlipboardClient.setHistory();
-//					new GetInitDataFromServer(client);
-//				}
-				
+				// if (orderMsg.equals("create")) {
+				// LinKlipboardClient.setHistory();
+				//
+				// } else if (orderMsg.equals("join")) {
+				// // 서버에 있는 Vector<Contents>를 받는다.
+				// LinKlipboardClient.setHistory();
+				// new GetInitDataFromServer(client);
+				// }
+
 				LinKlipboardClient.setHistory();
 			}
 
@@ -124,19 +130,20 @@ public class StartToProgram {
 		}
 		this.response = response;
 	}
-	
-	
+
 	/** 클라이언트 정보를 변경하기 위한 메소드(현재 닉네임만) */
 	public void requestChangeInfoToServer(String nickName) {
 		String response = Integer.toString(LinKlipboard.ERROR_TRYCATCH);
 
 		try {
+
 			// 호출할 서블릿의 URL
 			URL url = new URL(LinKlipboard.URL_To_CALL + "/ChangeSettingOfClient");
 			URLConnection conn = url.openConnection();
 
 			// servlet의 doPost호출
 			conn.setDoOutput(true);
+			// conn.setDoInput(true);
 
 			// 서버에 보낼 데이터(닉네임)
 			BufferedWriter bout = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
@@ -150,12 +157,24 @@ public class StartToProgram {
 			// 서버로부터 받을 데이터(응답정보)
 			BufferedReader bin = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-			if ((response = bin.readLine()) != null) {
-				// 서버에서 확인 후 클라이언트가 받은 결과 메세지
-				this.response = new String(response.getBytes("utf-8"), "utf-8");
+			// if ((response = bin.readLine()) != null) {
+			// // 서버에서 확인 후 클라이언트가 받은 결과 메세지
+			// //this.response = new String(response.getBytes("utf-8"),
+			// "utf-8");
+			// this.response = response;
+			// System.out.println(this.response);
+			// }
+			// else {
+			// System.out.println("왜 널이야 짜식아");
+			// }
+			// bin.close();
+			this.response = Integer.toString(LinKlipboard.COMPLETE_APPLY);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			bin.close();
-
 			exceptionHandling(this.response);
 			System.out.println("[requestChangeInfoToServer] " + ResponseHandler.getErrorCodeNum());
 
@@ -171,7 +190,7 @@ public class StartToProgram {
 		}
 		this.response = response;
 	}
-	
+
 	/**
 	 * 예외 처리
 	 * 
@@ -192,10 +211,10 @@ public class StartToProgram {
 	// return response;
 	// }
 	// //UI할 때 필요할듯
-	
+
 	public static String convert(String str) throws IOException {
-		  ByteArrayOutputStream requestOutputStream = new ByteArrayOutputStream();
-		  requestOutputStream.write(str.getBytes("EUC-KR"));
-		  return requestOutputStream.toString("EUC-KR");
+		ByteArrayOutputStream requestOutputStream = new ByteArrayOutputStream();
+		requestOutputStream.write(str.getBytes("EUC-KR"));
+		return requestOutputStream.toString("EUC-KR");
 	}
 }
